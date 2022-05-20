e68e95c367f3229dac2ad19c197cb0c1101842dd
Sergey Seleznev <reloqus@yandex.ru>
2022-05-20 17:02:43 +0700

    final stage 1.2

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,31 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let popupName = document.querySelector('.popup__input_name');
-let popupJob = document.querySelector('.popup__input_job');
 let formElement = document.querySelector('.popup__form');
+let profileJob = document.querySelector('.profile__job');
+let popupName = formElement.querySelector('.popup__input_type_name');
+let popupJob = formElement.querySelector('.popup__input_type_job');
+
 
 function formEditOpen() {
-    popup.classList.add('popup__open');
     popupName.value = profileName.textContent;
     popupJob.value = profileJob.textContent;
+    popup.classList.add('popup_open');
 };
 
 function formEditClose() {
-    popup.classList.remove('popup__open');
+    popup.classList.remove('popup_open');
 };
-
-editButton.addEventListener('click', formEditOpen);
-
-closeButton.addEventListener('click', formEditClose);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = popupName.value;
     profileJob.textContent = popupJob.value;
-    popup.classList.remove('popup__open');
+    formEditClose();
 };
+
+editButton.addEventListener('click', formEditOpen);
+
+closeButton.addEventListener('click', formEditClose);
 
 formElement.addEventListener('submit', formSubmitHandler);
